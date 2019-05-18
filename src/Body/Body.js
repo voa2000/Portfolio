944ca70a649aedbe0c8820705f004bf2e2ffb39e
voa2000 <voa2000@hotmail.com>
2019-05-18 08:00:44 +0100

    Created Project elements

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Body.css";
-import comp from "./../images/computer.jpg";
+import comp from "./../images/viv.jpg";
+import nodejs from "./../images/nodejs.png";
+import css3 from "./../images/css3.png";
+import heroku from "./../images/heroku.png";
+import mongodb from "./../images/monogodb.jpeg";
+import reactjs from "./../images/reactjs.png";
+import travis from "./../images/TravisCI.png";
 
 function Body() {
   return (
@@ -8,18 +14,26 @@ function Body() {
       <img src={comp} className="body-main-photo" alt="computer" />
       <div className="body-area">
         <div className="body-area-box">
-          A personal UX designer portfolio website is a way for you to be
-          yourself and tell your story. Here you'll find top 19 hand-selected
-          portfolios that will inspire you when building your own. When done
-          right, it will work for you as your agent. It will score interviews
-          and bring home the job offers
+          <h1>Vivienne Owusu-Ansah</h1>
+          <h2>Software Engineer</h2>
         </div>
         <div className="body-area-box">
-          A personal UX designer portfolio website is a way for you to be
-          yourself and tell your story. Here you'll find top 19 hand-selected
-          portfolios that will inspire you when building your own. When done
-          right, it will work for you as your agent. It will score interviews
-          and bring home the job offers. Lorem
+          Enthusiastic Software Developer and Digital Educator with a passion
+          for the latest digital trends and open source technologies with over
+          20 years IT experience. <br />I have good problem-solving skills in
+          various technologies especially Modern JavaScript (React), I have the
+          ability to communicate effectively whilst discussing technical
+          problems with colleagues and clients at various levels.
+          <p />
+          <p />
+          <div className="small-images">
+            <img src={nodejs} alt="javascript" />
+            <img src={css3} alt="javascript" />
+            <img src={heroku} alt="javascript" />
+            <img src={mongodb} alt="javascript" />
+            <img src={reactjs} alt="javascript" />
+            <img src={travis} alt="javascript" />
+          </div>
         </div>
       </div>
     </div>
