b93a6132030ffb61f8f4f7be14f956437fbf3c2b
voa2000 <voa2000@hotmail.com>
2019-05-24 15:01:08 +0100

    link portfolio

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,17 +8,27 @@ export default function Landing() {
       <div className="body-area">
         <h1>Projects</h1>
         <div className="grid">
-          <div>
-            This is an application created using React, javascript, HTML, CSS,
-            Express and Mongodb.
-          </div>
-          <div style={{ alignItems: "center" }}>
-            <img
-              style={{ height: "275px", width: "375px" }}
-              src={kodflix}
-              alt="CSS Logo"
-            />
-          </div>
+          <card>
+            <div>
+              This is an application created using React, javascript, HTML, CSS,
+              Express and Mongodb.
+              <p />
+            </div>
+            <div>
+              <center>
+                <a href="http://vivkodflix.herokuapp.com/">
+                  <img
+                    style={{ height: "275px", width: "375px" }}
+                    src={kodflix}
+                    alt="CSS Logo"
+                  />
+                </a>
+                <p />
+                GitHub Link | Live Site
+              </center>
+            </div>
+            <div />
+          </card>
         </div>
       </div>
     </div>
