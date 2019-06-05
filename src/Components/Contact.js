7ce632941fd40a837ff5fdd8d3aa0606064e6e9e
voa2000 <voa2000@hotmail.com>
2019-06-05 12:32:42 +0100

    Moved footer elements to contact page.

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./About.css";
+import "./Footer.css";
 export default function Contact() {
   return (
     <div className="contact-container">
@@ -11,6 +12,22 @@ export default function Contact() {
           You can contact me using the links on the footer or at
           vivienneoansah@gmail.com
         </h2>
+        <div className="Footer-container">
+          <div className="social-links ">
+            <a href="https://www.linkedin.com/in/vivienne-o-6bb8364a/">
+              <i className="fab fa-linkedin fa-2x" aria-hidden="true" />
+            </a>
+            <a href="https://github.com/voa2000">
+              <i className="fab fa-github fa-2x" aria-hidden="true" />
+            </a>
+            <a href="https://twitter.com/voa2000">
+              <i className="fab fa-twitter fa-2x" aria-hidden="true" />
+            </a>
+            <a href="https://skype.com/voa2000">
+              <i className="fab fa-skype fa-2x" aria-hidden="true" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
