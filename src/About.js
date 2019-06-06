d1266a50fba0d8a8e7c9bae4576a2a83396bfde0
voa2000 <voa2000@hotmail.com>
2019-06-06 18:53:51 +0100

    Added an icon to

--- src/About.js
+++ src/About.js
@@ -8,24 +8,32 @@ export default function About() {
         <h2>
           Enthusiastic Software Developer and Digital Educator with a passion
           for the latest digital trends and open source technologies with over
-          20 years IT experience. I have good problem-solving skills in various
-          technologies especially Modern JavaScript (React), I have the ability
-          to communicate effectively whilst discussing technical problems with
-          colleagues and clients at various levels.
+          20 years IT experience.{" "}
+        </h2>
+        <h2>
+          I have good problem-solving skills in various technologies especially
+          using Modern JavaScript and React to build mobile responsive
+          applications, I have the ability to communicate effectively whilst
+          discussing technical problems with colleagues and clients at various
+          levels.
         </h2>
         <h2>Below is my Tech Stack</h2>
         <div className="about-techstack">
           <div className="about-techstack-item">
-            <i className="fab fa-react fa-3x" aria-hidden="true" />
+            <i className="fab fa-react fa-4x" aria-hidden="true" />
           </div>
           <div className="about-techstack-item">
-            <i className="fab fa-html5 fa-3x" aria-hidden="true" />
+            <i className="fab fa-html5 fa-4x" aria-hidden="true" />
           </div>
           <div className="about-techstack-item">
-            <i className="fab fa-node fa-3x" aria-hidden="true" />
+            <i className="fab fa-node fa-4x" aria-hidden="true" />
           </div>
           <div className="about-techstack-item">
-            <i className="fab fa-css3 fa-3x" aria-hidden="true" />
+            <i className="fab fa-css3 fa-4x" aria-hidden="true" />
+          </div>
+
+          <div className="about-techstack-item">
+            <i className="fab fa-aws fa-4x" aria-hidden="true" />
           </div>
         </div>
       </div>
